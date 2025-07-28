d0b601c665b81f4480f4080a71dae080a3fad4a3
Hamzasaeed1234567 <hamza.saeed@packagex.io>
2025-07-28 17:51:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
-
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useRoutes, useLocation } from "react-router-dom";
 import routes from "./routes";
+
 const App = () => {
   const content = useRoutes(routes);
+
+  // Disable right-click globally
+  useEffect(() => {
+    const handleContextMenu = (e) => e.preventDefault();
+    document.addEventListener("contextmenu", handleContextMenu);
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
 
   return (
     <React.Fragment>
@@ -18,10 +25,8 @@ const App = () => {
 export default App;
 
 const ScrollToTop = () => {
-  // Extracts pathname property(key) from an object
   const { pathname } = useLocation();
 
-  // Automatically scrolls to top whenever pathname changes
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
